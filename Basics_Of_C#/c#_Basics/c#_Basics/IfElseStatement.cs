c4ee9ea19a724c47d0cd6e65563ff5f960cd2f7c
dheepan036 <dheepantoreeto@gmail.com>
2025-08-27 12:06:33 +0530

    If else statement

---
+++ Basics_Of_C#/c#_Basics/c#_Basics/IfElseStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__Basics
+{
+    public static class IfElseStatement
+    {
+        public static void EligibleForVoting()
+        {
+            Console.WriteLine("Hi, Kindly enter your age to know whether you are eligible for voting:");
+            int age =int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible for voting");
+                if (age == 18)
+                {
+                    Console.WriteLine("First time voters: Vote responsibly");
+                }
+            }
+            else if (age == 17)
+            {
+                Console.WriteLine("Wait for just one year");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible for voting");
+            }
+            Console.ReadKey();
+        }
+    }
+}
