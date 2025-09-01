9168b5cf43fc0220987a5ca31d2e8ed1379f3436
dheepan036 <dheepantoreeto@gmail.com>
2025-09-01 11:42:56 +0530

    Basic quiz app

---
+++ QuizApp/QuizApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace QuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the quiz app");
+            int score = 0;
+            string question1 = "Who directed Avengers Endgame";
+            string answer1 = "Russo Brothers";
+
+            string question2 = "Who directed No country for old man";
+            string answer2 = "Coen Brothers";
+
+            string question3 = "Who directed Retro";
+            string answer3 = "Karthik subbaraj";
+
+            Console.WriteLine($"Question :{question1}");
+            string userAnswer = Console.ReadLine();
+            if(userAnswer == answer1)
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine($"Wrong answer. The Correct answer is {answer1}");
+            }
+            Console.WriteLine("Your Total score is " + score);
+            Console.ReadKey();
+        }
+    }
+}
