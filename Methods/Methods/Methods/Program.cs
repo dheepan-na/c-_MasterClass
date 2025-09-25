11c768038bf83fcc3d7f7800edf9eaf264f2865a
dheepan-na <nadheepan2@gmail.com>
2025-09-25 17:50:39 +0530

    Creating first method.

--- Methods/Methods/Methods/Program.cs
+++ Methods/Methods/Methods/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Creating my first method an calling it.");
+            void MyfirstMethod()
+            {
+                Console.WriteLine(" This is my first method");
+                Console.WriteLine("Methods are used to execute block of code whenever it is called.");
+            }
+            MyfirstMethod();
+            Console.WriteLine("Calling method for second time");
+            MyfirstMethod();
         }
     }
 }
