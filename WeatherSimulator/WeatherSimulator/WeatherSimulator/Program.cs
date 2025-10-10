b5290d934ce63ae528a92f55183f714c472b80ad
dheepan-na <nadheepan2@gmail.com>
2025-10-10 12:03:10 +0530

    Adding methods to calculate the temperature randomly

--- WeatherSimulator/WeatherSimulator/WeatherSimulator/Program.cs
+++ WeatherSimulator/WeatherSimulator/WeatherSimulator/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace WeatherSimulator
+﻿using System.Collections;
+
+namespace WeatherSimulator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of days to get weather");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] condition = { "Sunny", "Rainy", "Cloudy", "Windy", "Foggy" };
+            string[] dailyCondition = new string[days];
+
+            Random random = new Random();
+
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                dailyCondition[i] = condition[random.Next(condition.Length)];
+            }
+            Console.WriteLine("Displaying the temperature for the days");
+            DisplayWithObjectsMethod(temperature.Cast<Object>().ToArray());
+            DisplayWithGenericMethod(temperature);
+            DisplayWithIenumerableMethod(temperature);
+            Console.WriteLine("Displaying the weather condition for the days");
+
+
+
+            Console.ReadKey();
         }
+
+        static void DisplayWithObjectsMethod(object[] arr)
+        {
+            Console.WriteLine("Displaying with the objects method.");
+            foreach (object item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void DisplayWithGenericMethod<T>(T[] arr)
+        {
+            Console.WriteLine("Displaying with the Generics method.");
+            foreach (object item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void DisplayWithIenumerableMethod(IEnumerable arr)
+        {
+            Console.WriteLine("Displaying with the Ienumerable method. This type of displaying is mostly recommended.");
+            foreach (object item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
     }
 }
