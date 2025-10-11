b5ea641d9d7ebcadf6e616fa209b9e9ca7fceac0
dheepan-na <nadheepan2@gmail.com>
2025-10-11 14:03:38 +0530

    Custom constructor and member varible has been created.

--- ClassApp/ClassCar.cs
+++ ClassApp/ClassCar.cs
@@ -9,8 +9,18 @@ namespace ClassApp
     // this is an interanll class this can be called anywhere in the assembly ClassApp
     internal class ClassCar
     {
+        private string _modelOfCar;
+        private string _brandOfCar;
         public ClassCar() {
             Console.WriteLine("Here we have created a constructor. It will be called whenever the object of the class is created.");
+        }
+
+        public ClassCar(string modelOfCar, string brandOfCar)
+        {
+            Console.WriteLine("\nHere we have created a custom constructor with some variable.");
+            _modelOfCar = modelOfCar;
+            _brandOfCar = brandOfCar;
+            Console.WriteLine($"{_brandOfCar} has a model of {_modelOfCar} ");
         }
     }
 }
--- ClassApp/Program.cs
+++ ClassApp/Program.cs
@@ -9,6 +9,13 @@
             Console.WriteLine("New keyword allocates space in the memory for the class when it is created.");
             Console.WriteLine("\nInstance of the class is created. ");
             ClassCar car = new ClassCar();
+            Console.WriteLine("\nHere we have created a custom constructor with the member variable.");
+            Console.WriteLine("A member variable is the field or variable that is created inside the class and outside of all the membersof the class.");
+            Console.WriteLine("General rule for member variable is it should start with _ and it should be private.");
+            Console.WriteLine("Being private it is not accessible outside of the class using the object or instance.");
+            ClassCar audi = new ClassCar("Audi", "A3");
+            ClassCar bmw = new ClassCar("BMW", "i7");
+
             Console.ReadKey();
         }
     }
