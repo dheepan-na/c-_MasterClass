e01e3f0e261bccb5379eb8d6aac667f5c3581d99
dheepan-na <nadheepan2@gmail.com>
2025-10-11 13:42:23 +0530

    Created class, Constructor and instance.

---
+++ ClassApp/ClassCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    // this is an interanll class this can be called anywhere in the assembly ClassApp
+    internal class ClassCar
+    {
+        public ClassCar() {
+            Console.WriteLine("Here we have created a constructor. It will be called whenever the object of the class is created.");
+        }
+    }
+}
--- ClassApp/Program.cs
+++ ClassApp/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Class is a blueprint of methods and variable that is most specific to the class.");
+            Console.WriteLine("Object or instance can be created by using the new keyword.");
+            Console.WriteLine("New keyword allocates space in the memory for the class when it is created.");
+            Console.WriteLine("\nInstance of the class is created. ");
+            ClassCar car = new ClassCar();
+            Console.ReadKey();
         }
     }
 }
