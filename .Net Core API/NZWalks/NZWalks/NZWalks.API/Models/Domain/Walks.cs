6572f58c3a629e602a96160b205c737a6deaed09
dheepan-na <nadheepan2@gmail.com>
2025-12-02 10:15:48 +0530

    Adding non nullable navigation properties to the model. This would help for Entity Framework.

--- .Net Core API/NZWalks/NZWalks/NZWalks.API/Models/Domain/Walks.cs
+++ .Net Core API/NZWalks/NZWalks/NZWalks.API/Models/Domain/Walks.cs
@@ -16,6 +16,10 @@
 
         public Guid RegionId { get; set; }
 
+        //Navigation properties. Below we will create a navigation or relationship properties. Which will allow Entity framework core to create a 1-1 relationship between those tables.
+        public Difficulty Difficulty { get; set; }
+
+        public Regions Regions { get; set; }
 
 
     }
