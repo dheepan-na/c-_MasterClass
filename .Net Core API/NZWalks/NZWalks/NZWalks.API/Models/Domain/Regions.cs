b03eaef8ed21dd584b0a8b43b4dd7f77e3720c14
dheepan-na <nadheepan2@gmail.com>
2025-12-02 10:12:58 +0530

    Adding models to the API

---
+++ .Net Core API/NZWalks/NZWalks/NZWalks.API/Models/Domain/Regions.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Regions
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public string? RegionImageInUrl { get; set; }
+      
+    }
+}
