7ac0f466c0bcbd1883aa44512f38962720ae6d73
dheepan-na <nadheepan2@gmail.com>
2025-12-02 10:00:07 +0530

    Adding a new api for the students.

---
+++ .Net Core API/NZWalks/NZWalks/NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // To use this controller we have use routing like this https://localhost:portnumber/api/student. This will point to this controller.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly string[] students = new string[] { "Dheepan", "Yamuna", "Arumugam" };
+
+        //In this get is used to get all the details from the data or resource using this GET: https://localhost:portnumber/api/student. This will help to retreive all the data. 
+        [HttpGet]
+        public IActionResult GetStudentDetail()
+        {
+            // Here OK is the successfully return data to the client application.
+            return Ok(students);
+        }
+    }
+}
