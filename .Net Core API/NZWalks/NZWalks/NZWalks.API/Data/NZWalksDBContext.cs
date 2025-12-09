5efc039d468cbb0bb2b95b99f06f5a749d10c19f
dheepan-na <nadheepan2@gmail.com>
2025-12-09 08:55:33 +0530

    Adding DBContexxt

---
+++ .Net Core API/NZWalks/NZWalks/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Regions> Regions { get; set; }
+        public DbSet<Walks> Walks { get; set; }
+    }
+}
